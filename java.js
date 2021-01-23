920f08b06d055110ed13caf006dc87a21545d193
Bebe Lawrence <omlawrence24@gmail.com>
2021-01-23 13:50:20 -0500

    revised1

--- java.js
+++ java.js
@@ -1,4 +1,4 @@
-var todo;
+var todo; 
 
 function storetodo() {
     todo = $(this)
@@ -8,13 +8,31 @@ console.log("id:" + todoId  + " , task: " + userTodo)
 localStorage.setItem(todoId, userTodo);
 
 }
+
+
  function loadtodos() {
+    var taskDate = new Date()
+    var taskHour = taskDate.getHours()
+
      for (let i = 0; i < 24; i++) {
-         console.log("loading data for " + i) 
-        $("textarea#" + i).text(localStorage.getItem(i))
+         var slotHour = i + 1
+         var timeStatus = getHourStatus(taskHour,slotHour)
+        
+        //  console.log(`loading data for ${i} and hour Status is ${timeStatus}` ) 
+         console.log("loading data for " + slotHour + " and hour status is " + timeStatus)
+        $("textarea#" + slotHour).text(localStorage.getItem(slotHour))
+        debugger
+        $("textarea#" + slotHour).addClass(timeStatus)
      }
  }
- 
+  function getHourStatus (currentHour, testHour){
+    if (testHour < currentHour) return "past"
+    if (testHour > currentHour) return "future"
+    if (testHour === currentHour) return "present"
+    
+    return "present"
+
+  }
 
  $(document).ready(function(){
      $(".saveBtn").on("click", storetodo);
@@ -22,4 +40,4 @@ localStorage.setItem(todoId, userTodo);
 
  })
 
- 
+
